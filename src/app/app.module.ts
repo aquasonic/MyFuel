ddca5ce963900406994cfde86a89027bef832bd3
Kevin Brechbühl <mail@aquasonic.ch>
2019-10-19 14:33:47 +0200

    Bootstrap shell component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
+import { ShellComponent } from './components';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [ShellComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ShellComponent]
 })
 export class AppModule {}
