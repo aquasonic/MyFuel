c2e9eab355008b457d17d778b6486d0058a408b8
Kevin Brechbühl <mail@aquasonic.ch>
2019-11-20 21:29:43 +0100

    Bind list of fuels to array.

--- src/app/components/car-detail-view/car-detail-view.component.ts
+++ src/app/components/car-detail-view/car-detail-view.component.ts
@@ -9,7 +9,18 @@ import { ActivatedRoute } from '@angular/router';
 export class CarDetailViewComponent {
   name: string;
 
+  fuels = [
+    { date: new Date(), km: 134, litres: 24.33, consumption: 10.33, cost: 10.35 },
+    { date: new Date(), km: 134, litres: 24.33, consumption: 10.33, cost: 20.35 },
+    { date: new Date(), km: 134, litres: 24.33, consumption: 10.33, cost: 30.35 },
+    { date: new Date(), km: 134, litres: 24.33, consumption: 10.33, cost: 40.35 }
+  ];
+
   constructor(private route: ActivatedRoute) {
     this.name = this.route.snapshot.params.car;
+  }
+
+  addFuel() {
+    this.fuels.push({ date: new Date(), km: 134, litres: 24.33, consumption: 10.33, cost: 50.35 });
   }
 }
