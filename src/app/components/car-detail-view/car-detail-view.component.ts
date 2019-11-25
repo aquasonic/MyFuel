491d35e461452850aa1efe7b4baa0fb9103c9e27
Kevin Brechbühl <mail@aquasonic.ch>
2019-11-25 22:15:14 +0100

    Add actions to update and delete a car.

--- src/app/components/car-detail-view/car-detail-view.component.ts
+++ src/app/components/car-detail-view/car-detail-view.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CarDialogComponent } from '../car-dialog/car-dialog.component';
+import { ICar } from 'src/model';
+import { timer } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'myf-car-detail-view',
@@ -7,6 +11,8 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./car-detail-view.component.scss']
 })
 export class CarDetailViewComponent {
+  @ViewChild(CarDialogComponent, { static: false }) addCarDialog: CarDialogComponent;
+
   name: string;
 
   fuels = [
@@ -16,11 +22,23 @@ export class CarDetailViewComponent {
     { date: new Date(), km: 134, litres: 24.33, consumption: 10.33, cost: 40.35 }
   ];
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private router: Router, private route: ActivatedRoute) {
     this.name = this.route.snapshot.params.car;
   }
 
   addFuel() {
     this.fuels.push({ date: new Date(), km: 134, litres: 24.33, consumption: 10.33, cost: 50.35 });
+  }
+
+  updateCar() {
+    this.addCarDialog.openModal({ id: 'id', name: this.name } as ICar, car => timer(500).pipe(map(_ => car)));
+  }
+
+  deleteCar() {
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
+
+  onCarUpdated(car: ICar) {
+    this.name = car.name;
   }
 }
