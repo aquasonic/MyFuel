845147a959e43007bd8acaf7cf012ce9959287e7
Kevin Brechbühl <mail@aquasonic.ch>
2019-12-25 22:49:22 +0100

    Remove Netlify functions and Fauna dependencies.

--- src/app/components/home-view/home-view.component.ts
+++ src/app/components/home-view/home-view.component.ts
@@ -1,5 +1,4 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ClrForm } from '@clr/angular';
@@ -9,17 +8,14 @@ import { ClrForm } from '@clr/angular';
   templateUrl: './home-view.component.html',
   styleUrls: ['./home-view.component.scss']
 })
-export class HomeViewComponent implements OnInit {
-  message = 'Loading...';
-  numberOfUsers = 'Loading...';
-
+export class HomeViewComponent {
   @ViewChild(ClrForm, { static: true }) clrForm;
 
   form = new FormGroup({
     userId: new FormControl('', Validators.required)
   });
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private router: Router) {}
 
   submit() {
     if (this.form.invalid) {
@@ -27,17 +23,5 @@ export class HomeViewComponent implements OnInit {
     } else {
       this.router.navigate(['/', this.form.controls.userId.value]);
     }
-  }
-
-  ngOnInit(): void {
-    this.http.get<any>('/.netlify/functions/ping').subscribe(
-      result => (this.message = result.message),
-      error => (this.message = error.message)
-    );
-
-    this.http.get<any>('/.netlify/functions/users-count').subscribe(
-      result => (this.numberOfUsers = 'There are ' + result + ' users available.'),
-      error => (this.numberOfUsers = error.message)
-    );
   }
 }
