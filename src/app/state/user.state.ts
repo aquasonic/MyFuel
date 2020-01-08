8e93b66bef9c13147b21228c69c7beb74caae97e
Kevin Brechbühl <mail@aquasonic.ch>
2020-01-08 20:23:47 +0100

    Fix timestamp mapping.

--- src/app/state/user.state.ts
+++ src/app/state/user.state.ts
@@ -178,7 +178,7 @@ export class UserState {
   private toCar(data: any) {
     return {
       id: data.id,
-      timestamp: data.timestam,
+      timestamp: data.timestamp,
       name: data.name,
       dateOfPurchase: data.dateOfPurchase,
       mileageAtPurchase: data.mileageAtPurchase,
@@ -187,6 +187,13 @@ export class UserState {
   }
 
   private toFuel(data: any) {
-    return { id: data.id, timestamp: data.timestam, date: data.date, km: data.km, litres: data.litres, cost: data.cost } as Fuel;
+    return {
+      id: data.id,
+      timestamp: data.timestamp,
+      date: data.date,
+      km: data.km,
+      litres: data.litres,
+      cost: data.cost
+    } as Fuel;
   }
 }
