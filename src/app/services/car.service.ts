68c1ac4c7c1e9822ddd1dbd561ffcc29be86ece9
Kevin Brechbühl <mail@aquasonic.ch>
2020-03-19 21:09:46 +0100

    Fix NullReference exception.

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -110,7 +110,7 @@ export class CarService {
   }
 
   getTotalKm(car: Car) {
-    if (!car.fuels.length) {
+    if (!car.fuels || !car.fuels.length) {
       return 0;
     }
 
@@ -118,7 +118,7 @@ export class CarService {
   }
 
   getAverageConsumation(car: Car) {
-    if (!car.fuels.length) {
+    if (!car.fuels || !car.fuels.length) {
       return 0;
     }
 
